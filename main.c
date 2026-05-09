29ba404435d925339f1b1bda5f95ff7387519618
Jamal <jamal.work2021@outlook.com>
2026-05-09 20:15:21 +0100

    created wait_ms and hack_nasa

--- main.c
+++ main.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+void wait_ms(int ms) {
+    #ifdef _WIN32
+        Sleep(ms);
+    #else
+        usleep(ms * 1000);
+    #endif
+}
+
+
+
+
+
+
+
+void hack_nasa() {
+    printf("HACKING INTO NASA......");
+    wait_ms(1000);
+    printf("HACK SUCCESSFUL. NASA ACCESS GRANTED.");
+
+}
+
+
+
+
+
+
 int main() {
-    printf("---   Welcome to Fake CMD.   ---\n");
+    printf("---   Welcome to Fake CMD. Run 'help' to see commands.   ---\n");
     printf("---   https://github.com/j1mal/fake-cmd   ---\n");
 
 
 
 
 
-    printf(".. >\n");
+    printf("j1mal@FakeCMD DevKit ~\n$ ");
     getchar();
     return 0;
 }
