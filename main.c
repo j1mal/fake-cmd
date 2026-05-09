3aff02e60ed0e6b67c4f06b4220294a7daf9ce86
Jamal <jamal.work2021@outlook.com>
2026-05-09 19:32:19 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("---   Welcome to Fake CMD.   ---\n");
+    printf("---   https://github.com/j1mal/fake-cmd   ---\n");
+
+
+
+
+
+    printf(".. >\n");
+    getchar();
+    return 0;
+}
