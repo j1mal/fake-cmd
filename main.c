47622925dfe324705f79629bb7966118eb4d7234
Jamal <jamal.work2021@outlook.com>
2026-05-11 17:50:04 +0100

    tuff shi

--- main.c
+++ main.c
@@ -14,33 +14,55 @@ void wait_ms(int ms) {
     #endif
 }
 
-
-
-
-
-
-
 void hack_nasa() {
-    printf("HACKING INTO NASA......");
+    printf("HACKING INTO NASA......\n");
     wait_ms(1000);
-    printf("HACK SUCCESSFUL. NASA ACCESS GRANTED.");
+    printf("HACK SUCCESSFUL. NASA ACCESS GRANTED.\n");
 
+}
+
+void help() {
+    printf("Commands : help, hack, exit\n");
+}
+
+void execute_command(char *input){
+    
 }
 
 
 
 
 
+// cmd sys
+typedef void (*command_fn)();
+typedef struct {
+    char *name;
+    command_fn func;
+} command;
+
+command commands[] = {
+    {"help",help},
+    {"hack",hack_nasa}
+};
 
 int main() {
-    printf("---   Welcome to Fake CMD. Run 'help' to see commands.   ---\n");
-    printf("---   https://github.com/j1mal/fake-cmd   ---\n");
+    printf(">>>>>>>>>>   FakeCMD succesfully loaded. Run 'help' to see commands.\n");
+    printf(">>>>>>>>>>   Original source @ https://github.com/j1mal/fake-cmd\n\n");
+    char input[100];
+    while(1){
+        
+        printf("j1mal@FakeCMD DevKit ~ $ ");
+        scanf("%99s",input);
+        if (strcmp(input,"exit") == 0){
+            break;
+        }
+        
+
+    }
 
 
 
 
 
-    printf("j1mal@FakeCMD DevKit ~\n$ ");
-    getchar();
     return 0;
 }
